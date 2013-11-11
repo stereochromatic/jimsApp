d01d01afb11373e631e3104f4f082304b2271928
Andrew <andrewhazlett.development@gmail.com>
2013-11-10 22:09:29 -0500

    sorting everything into templates, currently very rough

--- jimsApp.js
+++ jimsApp.js
@@ -1,13 +1,37 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to jimsApp.";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+//  Template.select_view.is_selected_view = function (view) {
+//    return Session.get('selected_view') === view ? 'class="active"' : '';
+//  };
+
+  Template.jimsApp.helpers({
+    is_selected_view : function (view) {
+      return Session.get('selected_view') === view;
+    }
+  });
+
+  Template.select_view.helpers({
+    is_selected_view : function (view) {
+      return Session.get('selected_view') === view ? 'class="active"' : '';
+    }
+  });
+  
+  Template.select_view.events({
+    'click #select_queue' : function () {
+      console.log('setting view queue');
+      Session.set('select_view', 'queue');
+    },
+    'click #select_order' : function () {
+      console.log('setting view order');
+      Session.set('select_view', 'order');
+    },
+    'click #select_customer' : function () {
+      console.log('setting view customer');
+      Session.set('select_view', 'customer');
+    },
+    'click #select_scan' : function () {
+      console.log('setting view scan');
+      Session.set('select_view', 'scan');
     }
   });
 }
