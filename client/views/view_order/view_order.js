35b133afa9ad6a6a2e28b633351a65b795b628fe
Andrew <andrewhazlett.development@gmail.com>
2013-11-15 18:03:07 -0500

    cleaned up customer view and model, finished order view and model

---
+++ client/views/view_order/view_order.js
@@ -0,0 +1,11 @@
+Template.view_order.customers = function () {
+	return Customer.prototype.load_all();
+};
+
+Template.view_order.events(okCancelEvents('#create_order', {
+	ok: function (value, event){ 
+			if (event.type === 'keyup' && part.value && due.value && lot.value && customer.value
+				&& Order.prototype.create(part.value, due.value, lot.value, customer.value)) 
+				alert('order created successfully');
+			}
+	}));
