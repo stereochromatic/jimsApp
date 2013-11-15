a68159d020d1f43cada45ade3415bfb92e77d43e
Andrew <andrewhazlett.development@gmail.com>
2013-11-15 16:33:00 -0500

    moved main.html to client, added underscore package, fixed up Models, wrote client_helpers

---
+++ client/lib/client_helpers.js
@@ -0,0 +1,27 @@
+/* client/lib/client_helpers.js */
+
+/* from todos examle */
+okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+
+      } else if (evt.type === "keyup" && evt.which === 13 ||
+                 evt.type === "focusout") {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+
+  return events;
+};
