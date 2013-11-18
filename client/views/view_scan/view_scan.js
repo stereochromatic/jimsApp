f82cfff29dc04c0ffb0ac944af907b14df493c75
Andrew <andrewhazlett.development@gmail.com>
2013-11-17 19:31:05 -0500

    overrode toString for Order and Station, wrote scanner view

--- client/views/view_scan/view_scan.js
+++ client/views/view_scan/view_scan.js
@@ -1,1 +1,28 @@
+/* client/views/view_scan/view_scan.js */
 
+var station_selected, order_selected;
+
+Template.view_scan.orders = function () {
+
+	return Order.prototype.load()
+};
+
+Template.view_scan.stations = function () {
+
+	return Station.prototype.load()
+};
+
+Template.view_scan.events( _.extend( {
+		'change #scanner' : function () {
+			station_selected = new Station( JSON.parse( station.value ) );
+			order_selected = new Order( JSON.parse( order.value ) );
+		}
+	}, 
+	okCancelEvents( '#scanner', {
+		ok: function ( value, event ) {
+			if ( event.type === 'keyup' 
+					&& station_selected && order_selected
+					&& station_selected.scan( order_selected ) ) 
+					alert( 'order created successfully' );
+		}
+	} ) ) );
