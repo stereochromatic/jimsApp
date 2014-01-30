0030321ae973bfb85b0266c9b2dc7f2c320c7ad6
Andrew <andrewhazlett.development@gmail.com>
2014-01-30 10:32:16 -0500

    properly selecting part for scan

--- client/views/view_scan/view_scan.js
+++ client/views/view_scan/view_scan.js
@@ -11,6 +11,40 @@ Template.view_scan.stations = function () {
 	return Station.prototype.load()
 };
 
+Template.view_scan.parts = function () {
+	var result = [];
+	Orders.find({parts: {$ne: void 0}}).forEach(function (order) {
+		result = result.concat(order.parts);
+	});
+	return result;
+}
+
+Template.view_scan.part = function () {
+	var parts = Template.view_scan.parts();
+	var part = _.filter(parts, function (part) {
+		return part.id == order.value;
+	})[0];
+	return part; 
+}
+	
+Template.view_scan.btnClass = function () {
+	console.log('btnClass called');
+	console.log(JSON.stringify($('#order')));
+	/* var part = Orders.findOne({parts: {$elemMatch: {id: order.value}}});
+	console.log(part);
+	if ($('#order') && order.value) {
+		var qty = (JSON.parse(order.value) || {quantity: 0}).quantity;
+		if (scan_qty.value < qty) {
+			return 'btn-default';
+		} else if (scan_qty === qty) {
+			return 'btn-success';
+		} else {
+			return 'btn-danger';
+		}		
+	} */
+	return 'btn-danger';
+};
+
 // Declare client scanLog collection
 //ScanLog = new Meteor.Collection("scanLog");
 
@@ -26,22 +60,9 @@ Template.view_scan.rendered = function () {
 		"bScrollCollapse": false,});
 };
 
-Template.view_scan.btnClass = function () {
-	var qty = (JSON.parse(order.value) || {quantity: 0}).quantity;
-	if (scan_qty.value < qty) {
-		return 'btn-default';
-	} else if (scan_qty === qty) {
-		return 'btn-success';
-	} else {
-		return 'btn-danger';
-	}
-}
-
 Template.view_scan.events( {
 
 	'change #scanner' : function () {
-		order_selected = order.value ? new Order( JSON.parse( order.value ) ) : undefined;
-		
 		if ( station.value ) {
 			station_selected = new Station( Stations.findOne( { name : station.value } ) );
 		} else if ( station ) {
@@ -49,48 +70,41 @@ Template.view_scan.events( {
 		} else {
 			station_selected = undefined;
 		}
-
 		console.log( 'scanner options changed:' );
-		console.log( order_selected );
+		console.log( Template.view_scan.part() );
 		console.log( station_selected );
 	},
 
 	'click #submit' : function () {
 		var partNumber = order.value;
-
-		//console.log(order.part_number);
+		//clickonsole.log(order.part_number);
 		//var orderPart = order[4];
 		//console.log(orderPart);
 		//console.log( partNumber );
 		//console.log( Order.part_number );
 
-
 		// create the scanlog entry
-        var newScan = {
-            scan_number: partNumber,
-            scan_station: station.value,
-            qty: scan_qty.value,
-            time: moment().format('LLLL'),
-            name: Meteor.user().profile.name
-        };
-
-        console.log( newScan.scan_number);
+       	var newScan = {
+       	    scan_number: partNumber,
+       	    scan_station: station.value,
+       	    qty: scan_qty.value,
+       	    time: moment().format('LLLL'),
+       	    name: Meteor.user().profile.name
+       	};
+       	console.log( newScan.scan_number);
         console.log( newScan.scan_station );
-
         if ( station_selected && station_selected._id && order_selected ) {
 			station_selected.scan( order_selected, newScan.qty );
 			station_selected = order_selected = undefined;
 			order.value = 'Select an Order';
 			station.value = 'Select a Station';
 			//alert( 'Check-in successful' );
-		}
-
-
+		}	
 
 		//Insert into scan log
 		ScanLog.insert(newScan);
 		//Update Order with last scan 'time'
-		
+			
         alert("Item has been checked in. Thanks!");
 	}
 } );
